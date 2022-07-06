3ebfffdc348f7edc9ab93a5cbeece7928578d297
xuxixi0913 <1352086116@qq.com>
2022-07-06 17:36:44 +0800

    Update test.js

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,5 +1,6 @@
 // pages/test/test.js
 const app=getApp()
+
 Page({
 
   /**
@@ -13,7 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    
+    let promise =new Promise(function(resolve, reject) {
     wx.cloud.database().collection('exam').get({
       success:(res)=>{
         let bgs=res.data
@@ -21,17 +22,21 @@ Page({
         console.log('bgs.length'+bgs.length)
         for(let i=0;i<bgs.length;i++){
           console.log('app.globalData.idnumber'+app.globalData.idnumber)
-          console.log('bgs[i].sno'+bgs[i].sno)
+          console.log('bgs[i].sno'+bgs[i].sno+" i===="+i)
           if(app.globalData.idnumber==bgs[i].sno)
           {
             var cname=""
             var cnum=0
             wx.cloud.database().collection('course').get({
+              
               success:(ret)=>{
                 let cr=ret.data
                 for(let j=0;j<cr.length;j++){
+                  console.log('cr[j].cno:'+cr[j].cno)
+                  console.log('bgs[i].cno:'+bgs[i].cno+" i="+i)
                   if(cr[j].cno==bgs[i].cno)
                   {
+                    console.log('cr[j].cname'+cr[j].cname+"  j="+j)
                     cname=cr[j].cname;
                     cnum=cr[j].cnum;
                     break;
@@ -42,17 +47,37 @@ Page({
                   credit:cnum,
                   start:bgs[i].start,
                   end:bgs[i].end,
-                  place:bgs[i].place
+                  place:bgs[i].place,
+                  time:bgs[i].startExam
                 }
                 this.setData({
                   examList:that.data.examList.concat(tmp)
                 })
+                
               }
             })
           }
         }
+        
+        // examList.sort(this.compare(time))
+        // compare: function (){
+        //   return function(a,b){
+        //     var v1=a.startExam;
+        //     var v2=b.startExam;
+        //     return v1-v2;
+        //   }
+        // }
         }
     })
+    
+  });
+  promise.then(function() {
+    examList.sort(function(a,b){
+      return a.time-b.time;
+    })
+    console.log(examList);
+  }
+  )
   },
 
   /**
